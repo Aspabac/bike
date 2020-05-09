94fb8f3661058e375d021fd6198d6da018a3681f
Daigomizuki <daigo3262@gmail.com>
2020-05-10 00:18:09 +0900

    最低限の完成

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let c = document.createElement('canvas');
 let ctx = c.getContext('2d');
 c.width = 800;
-c.height =600;
+c.height = 400;
 
 document.body.appendChild(c);
 
@@ -25,6 +25,7 @@ let player = new function() {
     this.y = 0;
     this.ySpeed = 0;
     this.rot =0;
+    this.rSpeed =0;
 
     this.img = new Image();
     this.img.src ="images/moto01.png";
@@ -33,19 +34,47 @@ let player = new function() {
         let p1 = c.height - noise(t + this.x)*0.25;
         let p2 = c.height - noise(t + 5+ this.x)*0.25;
 
-
+        let grounded = 0
 
         if(p1 - 15 > this.y){
             this.ySpeed +=0.1;
         }else{
             this.ySpeed -= this.y - (p1 -15);
             this.y = p1 - 15;
+
+            grounded = 1;
         }
+
+
+        if(!playing || grounded && Math.abs(this.rot) > Math.PI *0.5){
+            playing = false;
+            this.rSpeed =5;
+            k.ArrowUp = 1;
+            this.x -= speed *5;
+        }
+
+        let angle = Math.atan2((p2 - 15) - this.y, (this.x + 5) -this.x);
+
+        // this.rot = angle;
 
         this.y += this.ySpeed;
 
+
+        if(grounded && playing) {
+            this.rot -= (this.rot - angle) * 0.5;
+            this.rSpeed = this.rSpeed - (angle - this.rot);
+          }
+      
+       
+        this.rSpeed += (k.ArrowLeft - k.ArrowRight) * 0.5;
+        this.rot -= this.rSpeed * 0.1 ;
+
+        if(this.rot > Math.PI)this.rot = -Math.PI;
+        if(this.rot < -Math.PI)this.rot = Math.PI;
+
         ctx.save();
         ctx.translate(this.x, this.y);
+        ctx.rotate(this.rot);
         ctx.drawImage(this.img, -15, -15, 30, 30);
 
         ctx.restore();
@@ -54,9 +83,14 @@ let player = new function() {
 
 
 let t = 0;
-function loop() {
+let speed = 0;
+let playing = true;
+let k = {ArrowUp:0, ArrowDown:0, ArrowLeft:0, ArrowRight:0};
 
-    t += 5;
+
+function loop() {
+    speed -= (speed - (k.ArrowUp - k.ArrowDown)) *0.1;
+    t += 10 *speed;
     ctx.fillStyle = '#19f';
     ctx.fillRect(0,0,c.width,c.height);
     
@@ -77,5 +111,8 @@ function loop() {
     player.draw();
     requestAnimationFrame(loop);
 }
+
+onkeydown = d => k[d.key] = 1;
+onkeyup = d => k[d.key] = 0;
 
 loop();
